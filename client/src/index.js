17d5f2c5c64603d0315a402518db4716ddf3379e
Dario <106130562+dariorojas6@users.noreply.github.com>
2023-03-01 20:52:51 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import axios from "axios";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 // dotenv.config();
 
 // axios.defaults.baseURL = "http://localhost:3001";
