2289f73b2acb3aabbad20e2b439e2b858d9abf17
Dario <106130562+dariorojas6@users.noreply.github.com>
2023-03-01 20:49:13 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,9 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import axios from "axios";
 import dotenv from "dotenv";
-dotenv.config();
+// dotenv.config();
 
-axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.baseURL = "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
